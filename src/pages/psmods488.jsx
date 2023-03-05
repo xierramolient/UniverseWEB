0c50eb4ddea3f542eeae3fd3d80a2747e4edcab0
Xierrambatukam <124773426+xierramolient@users.noreply.github.com>
2023-03-05 19:08:56 +0700

    Add files via upload

---
+++ src/pages/psmods488.jsx
@@ -0,0 +1,9 @@
+import clquConfig from "../../clqu.config";
+
+export default function Error({ statusCode }) {
+    return <div className="flex flex-col items-center justify-center py-56">
+        <h1 className="text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-primary to-white"></h1>
+        <p className="text-2xl text-gray-500 dark:text-gray-500">You dont have permission to view this page.</p>
+    </div>;
+}
+
