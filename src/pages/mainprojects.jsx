fcb11e9d1f779b3fbc1f7c4160faacafc177a38a
Xierrambatukam <124773426+xierramolient@users.noreply.github.com>
2023-03-05 23:54:21 +0700

    Update mainprojects.jsx

--- src/pages/mainprojects.jsx
+++ src/pages/mainprojects.jsx
@@ -52,9 +52,9 @@ export default function Projects() {
       </p>
 	                        <div>
                                 <a
-                                    className="flex gap-2 px-4 items-center w-full h-12 bg-gray-500/5 rounded-lg hover:bg-gray-500/10 transition-all duration-200 text-zinc-400 hover:text-black hover:dark:text-white"
+                                    href="/starrailps" className="flex gap-2 px-4 items-center w-full h-12 bg-gray-500/5 rounded-lg hover:bg-gray-500/10 transition-all duration-200 text-zinc-400 hover:text-black hover:dark:text-white"
                                 >
-								<i className="fas fa-project-diagram" />
+								<i href="/starrailps" className="fas fa-project-diagram" />
                                 <a href="/starrailps" className="text-zinc-400 hover:text-primary hover:dark:text-white">Star Rail (Server Unavailable)</a>
                                 </a>
                             </div>
