70b6572e1a6ead54eb4a50b55bb33b5aba8e7817
Xierrambatukam <124773426+xierramolient@users.noreply.github.com>
2023-03-05 12:30:13 +0700

    Update mainprojects.jsx

--- src/pages/mainprojects.jsx
+++ src/pages/mainprojects.jsx
@@ -66,6 +66,12 @@ export default function Projects() {
 																  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
        ‎ 
       </p>
+																	  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
+																	  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
 		                        <div>
                                 <a
                                     className="flex gap-2 px-4 items-center w-full h-12 bg-gray-500/5 rounded-lg hover:bg-gray-500/10 transition-all duration-200 text-zinc-400 hover:text-black hover:dark:text-white"
