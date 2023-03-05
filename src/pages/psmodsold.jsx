5f4cfe33cbe93d11ba478b5640984950c0420cf9
Xierrambatukam <124773426+xierramolient@users.noreply.github.com>
2023-03-05 19:04:07 +0700

    Update psmodsold.jsx

--- src/pages/psmodsold.jsx
+++ src/pages/psmodsold.jsx
@@ -1,8 +1,110 @@
-import clquConfig from "../../clqu.config";
+import { usePage } from 'context/page'
+import useSWR from 'hooks/useSWR'
+import Head from 'next/head'
+import Image from 'next/image'
+import { Transition, Dialog } from '@headlessui/react'
+import { Fragment } from 'react'
+import clquConfig from '../../clqu.config'
+import Button from 'components/Global/Button'
+import Carousel from "react-multi-carousel";
 
-export default function Error({ statusCode }) {
-    return <div className="flex flex-col items-center justify-center py-56">
-        <h1 className="text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-primary to-white"></h1>
-        <p className="text-2xl text-gray-500 dark:text-gray-500">You dont have permission to view this page.</p>
-    </div>;
+export default function Projects() {
+    const { page } = usePage();
+
+    const { data: $projects } = useSWR('/api/projects');
+    const projects = $projects?.data;
+
+    return <>
+        <div className="max-w-7xl mx-auto">
+            <div className="py-24">
+			        <h1 className="text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-primary to-white">XierraCFW 4.89</h1>
+									  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
+                   <h1 className="text-2xl font-bold">Download XierrCFW Theme Only (Without Plugins and Features)<span className="relative whitespace-nowrap text-primary"></span></h1>
+
+
+
+					<h1 className="text-4xl font-bold"><span className="relative whitespace-nowrap text-primary"></span> </h1>
+									  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
+	  	                        <div>
+                                <a
+                                    className="flex gap-2 px-4 items-center w-full h-12 bg-gray-500/5 rounded-lg hover:bg-gray-500/10 transition-all duration-200 text-zinc-400 hover:text-black hover:dark:text-white"
+                                >
+								<i className="fas fa-project-diagram" />
+                                <a href="https://download1519.mediafire.com/42am5wvfz7lgOKchQQFaj4DJQv7RhzfBveM80RyNnSXa3O0FZfzoRZug4biKRE7F3nrW_5UdeRCw4v2CxDtGc_l1He8Dig/qlyajnq44ykoznl/XierraCFW+Theme+Only+v4.89.rar" className="text-zinc-400 hover:text-primary hover:dark:text-white">Download</a>
+                                </a>
+                            </div>
+																  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
+	                     <h1 className="text-2xl font-bold">Buy XierraCFW Source Code (4.89)<span className="relative whitespace-nowrap text-primary"></span></h1>
+				   				  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
+                    <p className="text-xl mt-1">
+                        <i className="" />
+                    </p>
+
+	  
+	                        <div>
+                                <a
+                                    className="flex gap-2 px-4 items-center w-full h-12 bg-gray-500/5 rounded-lg hover:bg-gray-500/10 transition-all duration-200 text-zinc-400 hover:text-black hover:dark:text-white"
+                                >
+								<i className="fas fa-project-diagram" />
+                                <a href="/login" className="text-zinc-400 hover:text-primary hover:dark:text-white">Buy Now</a>
+                                </a>
+                            </div>
+											   				  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
+
+																  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
+	  																					  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
+	  	  																					  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
+	   
+	  																					  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
+	  	  																					  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
+	  
+	                        <div>
+                                <a
+                                    className="flex gap-2 px-4 items-center w-full h-12 bg-gray-500/5 rounded-lg hover:bg-gray-500/10 transition-all duration-200 text-zinc-400 hover:text-black hover:dark:text-white"
+                                >
+								<i className="" />
+                                <a href="/mainprojects" className="text-zinc-400 hover:text-primary hover:dark:text-white">Back</a>
+                                </a>
+                            </div>
+
+	
+                <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 mt-16">
+                    {projects?.map((project, index) => (
+                        <a key={index} href={project.domain} target="_blank" rel="noreferrer" className="cursor-pointer w-full bg-gray-500/5 p-4 rounded-lg transition-all duration-200 hover:bg-gray-500/10">
+                            <div className="h-36 w-full relative rounded-lg shadow-xl overflow-hidden">
+                                <img src={project.banner} className="absolute w-full h-full object-cover " />
+                            </div>
+                            <div className="mt-4">
+                                <p className="text-xl font-semibold">{project.name}</p>
+                                <p className="text-sm max-h-24 h-24 overflow-auto font-normal text-gray-500">{project.overview}</p>
+                            </div>
+
+                            <p className="text-xs italic mt-6"><i className="fal fa-globe mr-1" />{project.domain}</p>
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </div>
+
+    </>
 }
