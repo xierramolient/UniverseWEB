c114ae3bb884045feb2819857070a4a85e8b1523
Xierrambatukam <124773426+xierramolient@users.noreply.github.com>
2023-03-06 00:30:06 +0700

    Update xierracfw.jsx

--- src/pages/xierracfw.jsx
+++ src/pages/xierracfw.jsx
@@ -68,10 +68,10 @@ export default function Projects() {
 	  
 	                        <div>
                                 <a
-                                    href="psmods" className="flex gap-2 px-4 items-center w-full h-12 bg-gray-50/10 rounded-lg hover:bg-gray-500/50 transition-all duration-200 text-zinc-400 hover:text-black hover:dark:text-white"
+                                    href="psmods" className=" gap- px-7 items-center h-12 bg-gray-50/10 rounded-lg hover:bg-gray-500/50 transition-all duration-200 text-zinc-400 hover:text-black hover:dark:text-white"
                                 >
 								<i className="" />
-                                <a href="psmods" className="text-zinc-400 hover:text-primary hover:dark:text-white">Back</a>
+                                <a href="psmods" className="font-display text-1x1 hover:text-primary hover:dark:text-white">BACK</a>
                                 </a>
                             </div>
 
