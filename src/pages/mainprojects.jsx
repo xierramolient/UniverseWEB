75fc541de78dca1472afc38364689167a40ada83
Xierrambatukam <124773426+xierramolient@users.noreply.github.com>
2023-03-06 20:29:05 +0700

    Update mainprojects.jsx

--- src/pages/mainprojects.jsx
+++ src/pages/mainprojects.jsx
@@ -77,6 +77,12 @@ export default function Projects() {
 														  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
        ‎ 
       </p>
+	  <p className="text-xl mt-1">
+                       + XierraCFW Releases<i className="" />
+                    </p>
+																			  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
 	  
 	                        <div>
                                 <a
