5a165f77edff76b1c4650fdf30f5bed332c62c12
Xierrambatukam <124773426+xierramolient@users.noreply.github.com>
2023-03-06 23:16:20 +0700

    Update _document.jsx

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -11,7 +11,7 @@ export default function MyDocument() {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
                 <meta name="description" content="XierraPS" />
-                <link rel="icon" href="https://ps.yuuki.me/favicon.ico" type="image/x-icon"/>
+                <link rel="icon" href="https://www.xierraps.xyz/favicon.ico" type="image/x-icon"/>
             </Head>
             <body>
                 <Main />
