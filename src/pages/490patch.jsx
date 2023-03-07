43339e130b16b3857ed8804ae1a27a0824725aac
Xierrambatukam <124773426+xierramolient@users.noreply.github.com>
2023-03-07 11:45:04 +0700

    Update 490patch.jsx

--- src/pages/490patch.jsx
+++ src/pages/490patch.jsx
@@ -47,6 +47,48 @@ export default function Projects() {
                                 <a href="https://www.mediafire.com/file/ucn84i2zdbxtyxn/ps3-updatelist.txt/file?dkey=5g0naotkblb&r=1981" className="text-zinc-400 hover:text-primary hover:dark:text-white">Download</a>
                                 </a>
                             </div>
+																												  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
+								  	  <p className="text-xl mt-1">
+                       + How to Install :<i className="" />
+                    </p>
+													  	  <p className="text-xl mt-1">
+                       1.Download the Patch files<i className="" />
+                    </p>
+													  	  <p className="text-xl mt-1">
+                       2.Copy the Patch file into FAT32 Storage Devices (Flashdisk or HDD)<i className="" />
+                    </p>
+																		  	  <p className="text-xl mt-1">
+                       3.Open your MMCM or File Manager on PS3<i className="" />
+                    </p>
+																							  	  <p className="text-xl mt-1">
+                       4.Open the FAT32 Storage Devices <i className="" />
+                    </p>
+																												  	  <p className="text-xl mt-1">
+                       5.Copy the ps3-updatelist.txt<i className="" />
+                    </p>
+																																	  	  <p className="text-xl mt-1">
+                       6.Paste the ps3-updatelist into /dev_hdd0 <i className="" />
+                    </p>
+																																	  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
+								  	  <p className="text-xl mt-1">
+                       + How to Install (Video Tutorial) :<i className="" />
+                    </p>
+																										  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
+       ‎ 
+      </p>
+	  	                        <div>
+                                <a
+                                    href="/henpatch" className="flex gap-2 px-4 items-center w-full h-12 bg-gray-50/10 rounded-lg hover:bg-gray-500/50 transition-all duration-200 text-zinc-400 hover:text-black hover:dark:text-white"
+                                >
+								<i href="/henpatch" className="fas fa-project-diagram" />
+                                <a href="/henpatch" className="text-zinc-400 hover:text-primary hover:dark:text-white">Watch Video</a>
+                                </a>
+                            </div>
+
 																  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
        ‎ 
       </p>
