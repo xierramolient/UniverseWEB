4ebf758ea8896c300b2a2c525589bf5f17ddec0f
Xierrambatukam <124773426+xierramolient@users.noreply.github.com>
2023-03-08 17:11:26 +0700

    Update mainprojects.jsx

--- src/pages/mainprojects.jsx
+++ src/pages/mainprojects.jsx
@@ -100,10 +100,10 @@ export default function Projects() {
       </p>
 						                        <div>
                                 <a
-                                    href="https://www.xierra.online/XierraHEN" className="flex gap-2 px-4 items-center w-full h-12 bg-gray-50/10 rounded-lg hover:bg-gray-500/50 transition-all duration-200 text-zinc-400 hover:text-black hover:dark:text-white"
+                                    href="https://hen.xierraps.xyz" className="flex gap-2 px-4 items-center w-full h-12 bg-gray-50/10 rounded-lg hover:bg-gray-500/50 transition-all duration-200 text-zinc-400 hover:text-black hover:dark:text-white"
                                 >
-								<i href="https://www.xierra.online/XierraHEN" className="fas fa-project-diagram" />
-                                <a href="https://www.xierra.online/XierraHEN" className="text-zinc-400 hover:text-primary hover:dark:text-white">PS3 XierraHEN Installer (4.83 - 4.90)</a>
+								<i href="https://hen.xierraps.xyz" className="fas fa-project-diagram" />
+                                <a href="https://hen.xierraps.xyz" className="text-zinc-400 hover:text-primary hover:dark:text-white">PS3 XierraHEN Installer (4.83 - 4.90)</a>
                                 </a>
                             </div>
 																												  	        <p className="mx-auto max-w-4xl font-display text-1x1 text-gray-500/50 text-center">
